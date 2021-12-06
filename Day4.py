e0b0cffcc4cfdc834bc4319e84f7bb6226f653a9
Oskari Ahonen <ahonenoskari@gmail.com>
2021-12-06 16:54:32 +0200

    Lisätty kommentit

--- Day4.py
+++ Day4.py
@@ -1,11 +1,13 @@
 with open('./res/Day4.txt', 'r') as file:
     data = list(filter(None, (line.rstrip() for line in file)))
 
+# luodaan integereistä koostuva lista kutsuttavista luvuista
 calls = data[0].split(',')
 calls = list(map(int, calls))
 i = 1
 boards = []
 
+# luodaan laudat ja muutetaan arvot inteiksi
 while i < len(data):
     boards.append(data[i:i+5])
     i+=5
@@ -13,12 +15,14 @@ for i, board in enumerate(boards):
     for x, item in enumerate(board):
         boards[i][x] = list(map(int, item.split()))
 
+# Jos numero vastaa huudettua numeroa muutetaan se -1
 def replace_x(number, call):
     if number == call:
         return -1
     else:
         return number
 
+# Tarkistetaan onko lauta voittanut
 def check_numbers(board):
     # tarkistetaan vaaka
     for line in board:
@@ -34,6 +38,7 @@ def check_numbers(board):
 
     return None
 
+# etsitään voittaja lauta
 def run_numbers(calls, boards):
     for number in calls:
         for i, board in enumerate(boards):
@@ -43,6 +48,7 @@ def run_numbers(calls, boards):
                     return boards[i], number
     return None
 
+# etsitään lauta joka voittaa viimeisenä
 def run_numbers_last(calls, boards):
     boards_left = len(boards)
     last_to_win = None
@@ -55,11 +61,11 @@ def run_numbers_last(calls, boards):
                     last_to_win = boards[i]
                     boards[i] = None
                     boards_left -= 1
-                # return boards[i], number
             if boards_left == 0:
                 return last_to_win, number
     return None
 
+# part1 lasketaan voittajalaudan luku
 winning_board = run_numbers(calls,boards)
 sum_unmarked = 0
 for i in range(len(winning_board[0])):
@@ -68,6 +74,7 @@ for i in range(len(winning_board[0])):
             sum_unmarked += winning_board[0][i][n]
 print(sum_unmarked*winning_board[1])
 
+# part2 lasketaan viimeisen voittajan luku
 last_board = run_numbers_last(calls, boards)
 sum_unmarked = 0
 for i in range(len(last_board[0])):
